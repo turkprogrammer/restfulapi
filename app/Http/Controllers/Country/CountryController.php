8493e122c78904a5ca71ec91e59ceb63640b1f5c
DESKTOP-7AU1H3Q\Yunus <ostbat43@gmail.com>
2021-02-25 19:36:15 +0500

    получение JSON данных из таблицы

---
+++ app/Http/Controllers/Country/CountryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Country;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\CountryModel;
+
+class CountryController extends Controller
+{
+    public function country(){
+        return response()->json(CountryModel::get(), 200); //Получаем JSON ответ
+    }
+}
---
+++ app/Models/CountryModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CountryModel extends Model
+{
+    protected $table = "country_lang";
+    protected $fillable = [
+        'id',
+        'alias',
+        'name',
+        'name_en',
+    ];
+}
